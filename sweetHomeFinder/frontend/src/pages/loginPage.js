b3d5120a7dda592a088ec2663dec96ae1fb0a546
Alex Bernatowicz <bernatowicz1122@gmail.com>
2024-09-23 21:20:04 -0500

    Test Commit

--- sweetHomeFinder/frontend/src/pages/loginPage.js
+++ sweetHomeFinder/frontend/src/pages/loginPage.js
@@ -7,7 +7,6 @@ import loginBackground2 from '../assets/unsplash2.jpg';
 import { Link } from 'react-router-dom';
 
 function LoginPage() {
-<<<<<<< HEAD
   return (
     <div className="login-page" style={{ backgroundImage: `url(${loginBackground2})` }}>
       <div className="login-container">
@@ -20,27 +19,6 @@ function LoginPage() {
         {/* Optional Sign-Up link (Clerk handles routing) */}
         <div className="login-options">
           <a href="/signup">New to our shelter? Sign up</a>
-=======
-    return (
-      <div className="login-page" style={{ backgroundImage: `url(${loginBackground2})` }}>
-        <div className="login-container">
-          <h2>Login to Sweet Home Finder</h2>
-          <div className="pet-icon">🐾</div>
-          <form className="login-form">
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" required />
-            </div>
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" required />
-            </div>
-            <button type="submit">Find Your Perfect Pet</button>
-          </form>
-          <div className="login-options">
-          <Link to="/signup">New to our shelter? Sign up</Link>
-          </div>
->>>>>>> 1bd86e68acc1d4187f6cf66d60c64b3a0bc92317
         </div>
       </div>
     </div>
--- sweetHomeFinder/frontend/src/pages/signupPage.js
+++ sweetHomeFinder/frontend/src/pages/signupPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/pages/signupPage.js
 
 import React from 'react';
@@ -19,48 +18,10 @@ function SignUpPage() {
         {/* Optional Login link */}
         <div className="login-options">
           <a href="/login">Already have an account? Log in</a>
-=======
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/signupPage.scss';
-import signupBackground from '../assets/unsplash2.jpg';
-
-function SignupPage() {
-  return (
-    <div className="signup-page" style={{ backgroundImage: `url(${signupBackground})` }}>
-      <div className="signup-container">
-        <h2>Create an Account</h2>
-        <div className="pet-icon">🐾</div>
-        <form className="signup-form">
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" required />
-          </div>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" required />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" required />
-          </div>
-          <div className="input-group">
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input type="password" id="confirm-password" required />
-          </div>
-          <button type="submit">Sign Up</button>
-        </form>
-        <div className="signup-options">
-          <Link to="/login">Already have an account? Log in</Link>
->>>>>>> 1bd86e68acc1d4187f6cf66d60c64b3a0bc92317
         </div>
       </div>
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default SignUpPage;
-=======
-export default SignupPage;
->>>>>>> 1bd86e68acc1d4187f6cf66d60c64b3a0bc92317
