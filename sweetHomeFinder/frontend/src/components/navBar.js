bf5bd1eee421ebee226cfb42b3cca6a24a460a46
Dhimitri Dinella <ddine3@uic.edu>
2024-10-07 19:15:00 -0500

    search button added

--- sweetHomeFinder/frontend/src/components/navBar.js
+++ sweetHomeFinder/frontend/src/components/navBar.js
@@ -1,10 +1,13 @@
-// src/components/navBar.js
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
-import logo from '../assets/logo-no-background.png';
-import '../styles/navBar.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import logo from "../assets/logo-no-background.png";
+import "../styles/navBar.scss";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,18 +24,45 @@ function NavBar() {
         </div>
 
         {/* Hamburger menu icon */}
-        <div className={`menu-btn ${isOpen ? 'close' : ''}`} onClick={toggleMenu}>
+        <div
+          className={`menu-btn ${isOpen ? "close" : ""}`}
+          onClick={toggleMenu}
+        >
           <div className="btn-line"></div>
           <div className="btn-line"></div>
           <div className="btn-line"></div>
         </div>
 
         {/* Dropdown menu */}
-        <ul className={`navbar-links ${isOpen ? 'show' : 'hide'}`}>
-          <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-          <li><Link to="/quiz" onClick={toggleMenu}>Find Your Pet</Link></li>
-          <li><Link to="/about-us" onClick={toggleMenu}>About Us</Link></li>
-          
+        <ul className={`navbar-links ${isOpen ? "show" : "hide"}`}>
+          <li>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/quiz" onClick={toggleMenu}>
+              Find Your Pet
+            </Link>
+          </li>
+          <li>
+            <Link to="/about-us" onClick={toggleMenu}>
+              About Us
+            </Link>
+          </li>
+
+          {/* Search Button */}
+          <li>
+            <button className="search-button">
+              <input
+                type="text"
+                placeholder="Search"
+                className="search-input"
+              />
+              <span className="search-icon">🔍</span>
+            </button>
+          </li>
+
           {/* Signed-out users see the custom styled SignIn button */}
           <SignedOut>
             <li>
@@ -44,7 +74,9 @@ function NavBar() {
 
           {/* Signed-in users see their UserButton (profile management) */}
           <SignedIn>
-            <li><UserButton /></li>
+            <li>
+              <UserButton />
+            </li>
           </SignedIn>
         </ul>
       </div>
