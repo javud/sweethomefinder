5e24b0b7cf6c1c30f806cece1cfe18b7515564f5
Javid Uddin <muddi7@uic.edu>
2024-10-11 15:05:02 -0500

    updated aboutUs page with images

--- sweetHomeFinder/frontend/src/pages/aboutUsPage.js
+++ sweetHomeFinder/frontend/src/pages/aboutUsPage.js
@@ -1,63 +1,71 @@
 import React from "react";
 import "../styles/aboutUsPage.scss";
-import teamImage1 from "../assets/placeholder.jpg"; // Example image paths
-import teamImage2 from "../assets/placeholder.jpg";
-import teamImage3 from "../assets/placeholder.jpg";
-import teamImage4 from "../assets/dhimitri_dinella.jpg";
+import teamImage1 from "../assets/alex_nobg.png"; // Example image paths
+import teamImage2 from "../assets/javid_nobg.png";
+import teamImage3 from "../assets/graz_nobg.png";
+import teamImage4 from "../assets/dhimitri_nobg.png";
 
 function AboutUsPage() {
   return (
     <div className="about-us-page">
-      <h1>About Us</h1>
+      <h1>Meet the Team</h1>
       <div className="team-grid">
+        <a
+          href="https://www.linkedin.com/in/alex-bernatowicz/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
         <div className="team-member">
           <img src={teamImage1} alt="Alex Bernatowicz" />
           <h2>
-            <a
-              href="https://www.linkedin.com/in/alex-bernatowicz/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               Alex Bernatowicz
-            </a>
           </h2>
+          <p>Software Engineer</p>
+          <p className="flag">🇵🇱</p>
         </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/javiduddin/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
         <div className="team-member">
           <img src={teamImage2} alt="Javid Uddin" />
           <h2>
-            <a
-              href="https://www.linkedin.com/in/javiduddin/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               Javid Uddin
-            </a>
           </h2>
+          <p>Software Engineer</p>
+          <p className="flag">🇵🇰</p>
         </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/grazvydas-revuckas-99377116b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
         <div className="team-member">
           <img src={teamImage3} alt="Grazvydas Revuckas" />
           <h2>
-            <a
-              href="https://www.linkedin.com/in/grazvydas-revuckas-99377116b/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               Grazvydas Revuckas
-            </a>
           </h2>
+          <p>Software Engineer</p>
+          <p className="flag">🇱🇹</p>
         </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dhimitridinella/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
         <div className="team-member">
           <img src={teamImage4} alt="Dhimitri Dinella" />
           <h2>
-            <a
-              href="https://www.linkedin.com/in/dhimitridinella/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               Dhimitri Dinella
-            </a>
           </h2>
+          <p>Software Engineer</p>
+          <p className="flag">🇦🇱</p>
         </div>
+        </a>
       </div>
     </div>
   );
